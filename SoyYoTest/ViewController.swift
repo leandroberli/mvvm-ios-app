cca738a81f366014f45d6b53dcc4f7ed3a03e8dc
leandroberli@gmail.com <leandroberli@gmail.com>
2021-12-22 18:42:23 -0300

    Initial Commit

---
+++ SoyYoTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoyYoTest
+//
+//  Created by Leandro Berli on 22/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
